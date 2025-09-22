753b3e7fda00cdcdeea93c7579811e9450137ed6
Abhinav Kumar <114585720+ABHINAVKUMAR011@users.noreply.github.com>
2025-09-22 14:26:48 +0530

    Add files via upload
    comparison operator in js experiment with null and undefined

---
+++ comparison.js
@@ -0,0 +1,15 @@
+console.log(1>2)  // greater than  false
+console.log(1<2) // less than   true
+console.log(1==1) // equal to  true
+
+console.log("123" > 1)  // true
+console.log(" " > 1)    // false
+console.log(null>0) // false
+console.log(null==0) //false
+console.log(null>=0) //true
+
+
+console.log(undefined > 0)
+console.log(undefined <= 0) // anything with undefined return false
+console.log(undefined == 0)
+console.log(undefined == 1)
